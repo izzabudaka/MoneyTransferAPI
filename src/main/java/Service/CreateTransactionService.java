5a148da014fd37ec966f700faf39e1678c7e9b8e
Izz Abudaka <izz.abudaka@student.manchester.ac.uk>
2016-02-10 00:33:58 +0000

    Sanitise Amount value

--- src/main/java/Service/CreateTransactionService.java
+++ src/main/java/Service/CreateTransactionService.java
@@ -2,6 +2,7 @@ package Service;
 
 import Model.Database;
 import Model.Transaction;
+import Utility.CreateTransactionException;
 import io.vertx.core.json.Json;
 
 import java.sql.SQLException;
@@ -13,6 +14,9 @@ public class CreateTransactionService {
 
     public int createTransaction(String transactionDetails) throws SQLException {
         Transaction transaction = Json.decodeValue(transactionDetails, Transaction.class);
+        if(transaction.getAmount() <= 0)
+            throw new CreateTransactionException(
+                    String.format("You cannot make a transaction with amount %d", transaction.getAmount()));
         Database.insertIntoTransactions(transaction.getId(), transaction.getSender(),
                                         transaction.getReceiver(), transaction.getAmount());
         return transaction.getId();
